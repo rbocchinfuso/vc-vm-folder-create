9faf86a93dc32f2dee212bc6abc97a3e88f091b7
bocchrj <bocchrj@CORP.AMAZONWORKSPACES.COM>
2019-11-27 13:11:58 -0500

    update

--- move_vms.ps1
+++ move_vms.ps1
@@ -42,8 +42,58 @@ Import-Csv $vmFolderMap -UseCulture | %{
     $folder = Get-FolderByPath -Name $_.FolderName
     Move-VM -VM $vm -Destination $folder
 }
-
  
 # Disconnect from vCenter Server
 write-host "Disconnecting from vCenter Server $vcenter" -foreground green
 $vc = Disconnect-VIServer -Server $vcenter -Confirm:$false
+
+function Get-FolderByPath{
+    <#
+     .SYNOPSIS Retrieve folders by giving a path
+     .DESCRIPTION The function will retrieve a folder by it's path.
+       The path can contain any type of leave (folder or datacenter).
+     .NOTES
+       Author: Luc Dekens .PARAMETER Path The path to the folder. This is a required parameter.
+     .PARAMETER
+       Path The path to the folder. This is a required parameter.
+     .PARAMETER
+       Separator The character that is used to separate the leaves in the path. The default is '/'
+     .EXAMPLE
+       PS> Get-FolderByPath -Path "Folder1/Datacenter/Folder2"
+     .EXAMPLE
+       PS> Get-FolderByPath -Path "Folder1>Folder2" -Separator '>'
+    #>
+     
+      param(
+      [CmdletBinding()]
+      [parameter(Mandatory = $true)]
+      [System.String[]]${Path},
+      [char]${Separator} = '/'
+      )
+     
+      process{
+        if((Get-PowerCLIConfiguration).DefaultVIServerMode -eq "Multiple"){
+          $vcs = $global:defaultVIServers
+        }
+        else{
+          $vcs = $global:defaultVIServers[0]
+        }
+     
+        foreach($vc in $vcs){
+          $si = Get-View ServiceInstance -Server $vc
+          $rootName = (Get-View -Id $si.Content.RootFolder -Property Name).Name
+          foreach($strPath in $Path){
+            $root = Get-Folder -Name $rootName -Server $vc -ErrorAction SilentlyContinue
+            $strPath.Split($Separator) | %{
+              $root = Get-Inventory -Name $_ -Location $root -Server $vc -ErrorAction SilentlyContinue
+              if((Get-Inventory -Location $root -NoRecursion | Select -ExpandProperty Name) -contains "vm"){
+                $root = Get-Inventory -Name "vm" -Location $root -Server $vc -NoRecursion
+              }
+            }
+            $root | where {$_ -is [VMware.VimAutomation.ViCore.Impl.V1.Inventory.FolderImpl]}|%{
+              Get-Folder -Name $_.Name -Location $root.Parent -NoRecursion -Server $vc
+            }
+          }
+        }
+      }
+    }
